326e40ac6c47c841340efec182970b7714ebd2f6
Mohammed Farah <m.farah009@gmail.com>
2018-11-10 15:51:01 +0200

    remove bar'

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import UserForm from './components/UserForm';
 import CssBaseline from '@material-ui/core/CssBaseline'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CssBaseline />
-        <AppBar postion="static">
-            <Toolbar>
-              <Typography variant="h6" color="inherit">
-                Enter User Details
-              </Typography>
-            </Toolbar>
-        </AppBar>
         <UserForm />
       </div>
     );
